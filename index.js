991ef07d3c7e967e39742dbe31f94ce1a042b6d1
lakshya <dhawanlakshya04@gmail.com>
2026-02-27 15:01:29 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express")
+const app = express();
+
+app.use("");
+
+app.post("user/signup", (req,res) => {
+    
+});
+
+app.post("user/login", (req,res) => {
+    
+});
+
+app.post("course/purchase", (req,res) => {
+    
+});
+
+app.get("user/purchases", (req,res) => {
+    
+});
+
+app.get("/courses", (req,res) => {
+    
+});
+
+app.listen(3000);
