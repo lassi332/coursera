db73da4ce7660c34f007bb13023d6ec1b3496803
lakshya <dhawanlakshya04@gmail.com>
2026-03-10 08:25:40 +0530

    minimal changes

--- index.js
+++ index.js
@@ -3,18 +3,16 @@ const mongoose = require("mongoose");
 const app = express();
 const { userRouter } = require("./router/user")
 const { courseRouter } = require("./router/course")
-const { purchaseRouter } = require("./router/purchase")
 const { adminRouter } = require("./router/admin")
 
 app.use(express.json());
 
 app.use("/user", userRouter);
 app.use("/course", courseRouter);
-app.use("/purchase", purchaseRouter);
 app.use("/admin", adminRouter);
 
 async function main(){
-    await mongoose.connect("mongodb+srv://dhawanlakshya04:xjxuagwF9NSGNGwK@cluster0.h1gr9kw.mongodb.net/")
+    await mongoose.connect("mongodb+srv://dhawanlakshya04:xjxuagwF9NSGNGwK@cluster0.h1gr9kw.mongodb.net/coursera")
     console.log("db connected");
     app.listen(3000);
 }
