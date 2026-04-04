589d50d734b0a5871cdfafe8a2f6070927be008f
lakshya <dhawanlakshya04@gmail.com>
2026-04-04 12:34:46 +0530

    added signin and signup endpoints in admin router

--- router/admin.js
+++ router/admin.js
@@ -1,11 +1,80 @@
-const express = require("express");
-const adminRouter = express.Router();
 const {adminModel} = require("../db");
+const {z} = require("zod");
+const bcrypt = require('bcrypt');
+const jwt = require("jsonwebtoken");
 
-adminRouter.post("/", (req, res) => {
-    
+const signupSchema = z.object({
+    email: z.email(),
+    password: z.string().min(6),
+    firstname: z.string().min(1),
+    lastname: z.string().min(1)
 })
-adminRouter.post("/", (req, res) => {
+const signinSchema = z.object({
+    email: z.email(),
+    password: z.string().min(6),
+})
+
+adminRouter.post("/signup", async (req, res) => {
+    const parsedData = signupSchema.safeParse(req.body);
+    if (!parsedData.success){
+        return res.status(400).json({
+            message: "invalid input"
+        })
+    }
+    const {email, password, firstname, lastname} = parsedData.data;
+    const hashedPass = await bcrypt.hash(password, 5);
+
+    const ins = await adminModel.create({
+        email,
+        password: hashedPass,
+        firstname,
+        lastname
+    });
+
+    if (!ins){
+        res.status(400).json({
+            message: "couldnt store data on db"
+        })
+    }
+    res.json({
+        message: "signup successfull"
+    })
+    
+
+})
+
+adminRouter.post("/signin", async (req, res) => {
+    const parsedData = signinSchema.safeParse(req.body);
+    if (!parsedData.success){
+        return res.status(400).json({
+            message: "invalid creds"
+        })
+    }
+
+    const {email, password} = parsedData.data;
+    const hashedPass = await bcrypt.hash(password, 5);
+
+    const usr = await adminModel.findOne({
+        email,
+    });
+
+    if (!usr){
+        return res.status(400).json({
+            message: "no admin found"
+        })
+    }
+
+    const isPassCorrect = await bcrypt.compare(password, usr.password);
+    if (!isPassCorrect){
+        return res.status(400).json({
+            message: "wrong pass"
+        });
+    }
+
+    const token = jwt.sign(usr._id.toString(), process.env.ADMIN_JWT_KEY);
+    res.json({
+        token: token
+    })
 
 })
 
